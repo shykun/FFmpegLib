38410b168b8eddce22a50a63daa51ee44aa5ed92
Volodymyr Shykun <shykun@mlsdev.com>
2019-05-31 15:02:43 +0300

    added FFmpegAudioVideoMerger

--- ffmpeglib/src/main/java/com/shykun/volodymyr/ffmpeglib/ffmpeg/video/FFmpegAudioVideoMerger.kt
+++ ffmpeglib/src/main/java/com/shykun/volodymyr/ffmpeglib/ffmpeg/video/FFmpegAudioVideoMerger.kt
@@ -1,4 +1,4 @@
-package com.shykun.volodymyr.ffmpeglib.ffmpeg.image
+package com.shykun.volodymyr.ffmpeglib.ffmpeg.video
 
 import android.content.Context
 import android.net.Uri
@@ -9,66 +9,75 @@ import com.shykun.volodymyr.ffmpeglib.ContentType
 import com.shykun.volodymyr.ffmpeglib.ffmpeg.FFMpegCallback
 import com.shykun.volodymyr.ffmpeglib.getConvertedFile
 import com.shykun.volodymyr.ffmpeglib.getPath
-import java.io.File
 import java.io.IOException
 
-class FFmpegImagesFromVideo(private val context: Context) {
+class FFmpegAudioVideoMerger private constructor(private val context: Context) {
 
+    private var audioUri: Uri? = null
     private var videoUri: Uri? = null
     private var callback: FFMpegCallback? = null
     private var outputPath = ""
     private var outputFileName = ""
-    private var interval = 1.0
 
-    fun setVideoUri(videoUri: Uri): FFmpegImagesFromVideo {
+    fun setAudioUri(audioUri: Uri): FFmpegAudioVideoMerger {
+        this.audioUri = audioUri
+        return this
+    }
+
+    fun setVideoUri(videoUri: Uri): FFmpegAudioVideoMerger {
         this.videoUri = videoUri
         return this
     }
 
-    fun setCallback(callback: FFMpegCallback): FFmpegImagesFromVideo {
+    fun setCallback(callback: FFMpegCallback): FFmpegAudioVideoMerger {
         this.callback = callback
         return this
     }
 
-    fun setOutputPath(output: String): FFmpegImagesFromVideo {
+    fun setOutputPath(output: String): FFmpegAudioVideoMerger {
         this.outputPath = output
         return this
     }
 
-    fun setOutputFileName(output: String): FFmpegImagesFromVideo {
+    fun setOutputFileName(output: String): FFmpegAudioVideoMerger {
         this.outputFileName = output
         return this
     }
 
-    fun setInterval(interval: Double): FFmpegImagesFromVideo {
-        this.interval = interval
-        return this
-    }
-
-    fun extract() {
-        val inputLocation = getPath(context, videoUri!!)
-        val outputLocation = getConvertedFile(outputPath, "")
+    fun execute() {
+        val outputLocation = getConvertedFile(outputPath, outputFileName)
+        val audioPath = getPath(context, audioUri!!)
+        val videoPath = getPath(context, videoUri!!)
 
         val cmd = arrayOf(
             "-i",
-            inputLocation!!,
-            "-r",
-            interval.toString(),
-            outputLocation.path + File.separator + outputFileName + "_%04d.jpg"
+            videoPath,
+            "-i",
+            audioPath,
+            "-c:v", "copy",
+            "-c:a",
+            "aac",
+            "-strict",
+            "experimental",
+            "-map",
+            "0:v:0",
+            "-map",
+            "1:a:0",
+            "-shortest",
+            outputLocation.path
         )
 
         try {
             FFmpeg.getInstance(context).execute(cmd, object : ExecuteBinaryResponseHandler() {
-                override fun onStart() {
-                    callback?.onStart()
-                }
+                override fun onStart() {}
 
                 override fun onProgress(message: String?) {
                     callback?.onProgress(message!!)
                 }
 
                 override fun onSuccess(message: String?) {
-                    callback?.onSuccess(outputLocation, ContentType.IMAGES)
+                    callback?.onSuccess(outputLocation, ContentType.VIDEO)
+
                 }
 
                 override fun onFailure(message: String?) {
